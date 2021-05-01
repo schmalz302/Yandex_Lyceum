9207ecaf69fdc5bf401358944a9bbd0df3b89d18
buk55 <boltneffe@yandex.ru>
2021-05-01 14:17:07 +0500

    Большая задача по Maps API. Часть №3

--- 1.py
+++ 1.py
@@ -1,118 +1,65 @@
+import requests
 import pygame
-from copy import deepcopy
-from random import choice
+import os
+api_server = "http://static-maps.yandex.ru/1.x/"
 
+lon = 56.045221
+lat = 53.419472
+delta = 0.002
 
-class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for i in range(height)]
-        # значения по умолчанию
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-        self.stop = False
+params = {
+    "ll": ",".join([str(lon), str(lat)]),
+    "spn": ",".join([str(delta), str(delta)]),
+    "l": "map"
+}
+response = requests.get(api_server, params=params)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, screen):
-        y = self.top
-        for i in range(self.height):
-            x = self.left
-            s = self.cell_size
-            for j in range(self.width):
-                if self.board[i][j] == 1:
-                    pygame.draw.rect(screen, pygame.Color('green'), (x + 1, y + 1, self.cell_size - 1,
-                                                                     self.cell_size - 1), 0)
-                pygame.draw.rect(screen, pygame.Color('white'), (x, y, self.cell_size, self.cell_size), 1)
-                x += self.cell_size
-            y += self.cell_size
-
-    def get_cell(self, mouse_pos):
-        if self.left <= mouse_pos[0] <= self.left + self.cell_size * self.width and \
-        self.top <= mouse_pos[1] <= self.top + self.cell_size * self.height:
-            x = (mouse_pos[0] - self.left) // self.cell_size
-            y = (mouse_pos[1] - self.top) // self.cell_size
-            return (x, y)
-        else:
-            return None
-
-    def get_click(self, mouse_pos):
-        a = self.get_cell(mouse_pos)
-        j, i = a
-        if not a is None:
-            if self.board[i][j] == 1:
-                self.board[i][j] = 0
-            else:
-                self.board[i][j] = 1
-
-
-class Life(Board):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-
-    def next_move(self):
-        if self.stop:
-            c = deepcopy(self.board)
-            for i in range(self.height):
-                for j in range(self.width):
-                    n = 0
-                    w = self.width
-                    h = self.height
-                    n += [c[(i + 1) % w][j]].count(1)
-                    n += [c[(i - 1) % w][j]].count(1)
-                    n += [c[i][(j + 1) % h]].count(1)
-                    n += [c[i][(j - 1) % h]].count(1)
-                    n += [c[(i + 1) % w][(j + 1) % h]].count(1)
-                    n += [c[(i + 1) % w][(j - 1) % h]].count(1)
-                    n += [c[(i - 1) % w][(j + 1) % h]].count(1)
-                    n += [c[(i - 1) % w][(j - 1) % h]].count(1)
-                    if c[i][j] == 0 and n == 3:
-                        self.board[i][j] = 1
-                    if c[i][j] == 1 and n != 2 and n != 3:
-                        self.board[i][j] = 0
-
-
-board = Life(30, 30)
-board.set_view(20, 20, 18)
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Жизнь на Торе')
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+pygame.display.set_caption('Большая задача по Maps API. Часть №2')
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+b = False
 running = True
-MYEVENTTYPE = pygame.USEREVENT + 1
-a = 500
-pygame.time.set_timer(MYEVENTTYPE, a)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == MYEVENTTYPE:
-            board.next_move()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                board.get_click(pygame.mouse.get_pos())
-            if event.button == 3:
-                board.stop = True
-            if event.button == 4:
-                if a > 50:
-                    a -= 50
-                    if a < 50:
-                        a = 50
-                pygame.time.set_timer(MYEVENTTYPE, a)
-            if event.button == 5:
-                a += 50
-                pygame.time.set_timer(MYEVENTTYPE, a)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if board.stop:
-                    board.stop = False
-                else:
-                    board.stop = True
-    screen.fill((0, 0, 0))
-    board.render(screen)
+            if event.key == 1073741899:
+                delta *= 2
+                if delta > 10:
+                    delta = 10
+                b = True
+            if event.key == 1073741902:
+                delta /= 2
+                if delta < 0:
+                    delta = 0
+                b = True
+            if event.key == pygame.K_LEFT:
+                lon -= 0.001
+                b = True
+            elif event.key == pygame.K_RIGHT:
+                lon += 0.001
+                b = True
+            elif event.key == pygame.K_UP:
+                lat += 0.001
+                b = True
+            elif event.key == pygame.K_DOWN:
+                lat -= 0.001
+                b = True
+    if b:
+        b = False
+        params['spn'] = ",".join([str(delta), str(delta)])
+        params["ll"] = ",".join([str(lon), str(lat)])
+        response = requests.get(api_server, params=params)
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
+pygame.quit()
+os.remove(map_file)
