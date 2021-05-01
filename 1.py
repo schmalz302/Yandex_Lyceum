3ad6869bb974632ed1abb68fc230fe27ad2b28cf
buk55 <boltneffe@yandex.ru>
2021-05-01 13:46:39 +0500

    PIL 2.0

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QPainter, QColor, QPen
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog, QPushButton, QLineEdit
+from PIL import Image
+from PyQt5.QtGui import QPixmap, QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog, QPushButton
 
 SCREEN_SIZE = [400, 400]
 
@@ -12,73 +12,89 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(400, 400, 500, 500)
-        self.setWindowTitle('Квадрат-объектив — 1')
+        self.setGeometry(400, 400, 500, 300)
+        self.setWindowTitle('PIL 2.0')
 
-        self.btn = QPushButton('Показать', self)
-        self.btn.resize(100, 30)
-        self.btn.move(10, 10)
-        self.btn.clicked.connect(self.hello)
-
-        self.lbl1= QLabel(self)
-        self.lbl1.setText('side')
-        self.lbl1.move(350, 10)
-        self.edit1 = QLineEdit(self)
-        self.edit1.setFixedWidth(80)
-        self.edit1.setFixedHeight(30)
-        self.edit1.move(385, 10)
-
-        self.lbl2 = QLabel(self)
-        self.lbl2.setText('coeff')
-        self.lbl2.move(350, 50)
-        self.edit2 = QLineEdit(self)
-        self.edit2.setFixedWidth(80)
-        self.edit2.setFixedHeight(30)
-        self.edit2.move(385, 50)
-
-        self.lbl3 = QLabel(self)
-        self.lbl3.setText('n')
-        self.lbl3.move(350, 90)
-        self.edit3 = QLineEdit(self)
-        self.edit3.setFixedWidth(80)
-        self.edit3.setFixedHeight(30)
-        self.edit3.move(385, 90)
-
-        self.do_paint = False
-        self.pixmap = QPixmap()
         self.image = QLabel(self)
-        self.image.move(20, 120)
-        self.image.resize(300, 300)
+        self.image.move(250, 20)
+        self.image.resize(200, 200)
+
+        btn1 = QPushButton('R', self)
+        btn1.resize(200, 40)
+        btn1.move(40, 10)
+        btn1.clicked.connect(self.hello)
+
+        btn2 = QPushButton('G', self)
+        btn2.resize(200, 40)
+        btn2.move(40, 70)
+        btn2.clicked.connect(self.hello)
+
+        btn3 = QPushButton('B', self)
+        btn3.resize(200, 40)
+        btn3.move(40, 130)
+        btn3.clicked.connect(self.hello)
+
+        btn4 = QPushButton('ALL', self)
+        btn4.resize(200, 40)
+        btn4.move(40, 190)
+        btn4.clicked.connect(self.hello)
+
+        btn5 = QPushButton('Против часовой стрелки', self)
+        btn5.resize(200, 40)
+        btn5.move(40, 240)
+        btn5.clicked.connect(self.hello1)
+
+        btn6 = QPushButton('По часовой стрелке', self)
+        btn6.resize(200, 40)
+        btn6.move(260, 240)
+        btn6.clicked.connect(self.hello1)
+
+        self.fname = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
+        self.im = Image.open(self.fname)
+        p = self.im.load()
+        self.im1, self.im2, self.im3 = self.im.copy(), self.im.copy(), self.im.copy()
+        p1, p2, p3 = self.im1.load(), self.im2.load(), self.im3.load()
+        x, y = self.im.size
+        for i in range(x):
+            for j in range(y):
+                r, g, b = p[i, j]
+                p1[i, j] = r, 0, 0
+                p2[i, j] = 0, g, 0
+                p3[i, j] = 0, 0, b
+        self.im.save(self.fname)
+        self.image.setPixmap(QPixmap(self.fname))
+        self.n = [self.im1, self.im2, self.im3, self.im]
+        self.name = self.im.copy()
 
     def hello(self):
-        self.a = int(self.edit1.text())
-        self.b = float(self.edit2.text())
-        self.c = int(self.edit3.text())
-        self.m = [[[(20, 120), (20 + self.a, 120)], [(20 + self.a, 120), (20 + self.a, 120 + self.a)],
-                  [(20 + self.a, 120 + self.a), (20, 120 + self.a)], [(20, 120 + self.a), (20, 120)]]]
-        for i in range(self.c - 1):
-            self.d = []
-            a = int((self.a - self.a * (self.b ** (i + 1))) / 2)
-            b = int(self.a * (self.b ** (i + 1)))
-            d = 20 + a
-            m = 120 + a
-            self.d.append([(d, m), (d + b, m)])
-            self.d.append([(d + b, m), (d + b, m + b)])
-            self.d.append([(d + b, m + b), (d, m + b)])
-            self.d.append([(d, m + b), (d, m)])
-            self.m.append(self.d)
-        self.do_paint = True
-        self.repaint()
+        a = self.sender().text()
+        if a == 'R':
+            self.name = self.n[0].copy()
+            self.name.save(self.fname)
+        elif a == 'G':
+            self.name = self.n[1].copy()
+            self.name.save(self.fname)
+        elif a == 'B':
+            self.name = self.n[2].copy()
+            self.name.save(self.fname)
+        else:
+            self.name = self.n[3].copy()
+            self.name.save(self.fname)
+        self.image.setPixmap(QPixmap(self.fname))
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter(self.pixmap)
-            qp.begin(self)
-            qp.setPen(QPen(Qt.red, 2))
-            for i in range(self.c):
-                for j in range(4):
-                    qp.drawLine(*self.m[i][j][0], *self.m[i][j][1])
-            qp.end()
+    def hello1(self):
+        for i in range(len(self.n)):
+            if self.sender().text()[:2] == "По":
+                self.n[i] = self.n[i].transpose(Image.ROTATE_270)
+            else:
+                self.n[i] = self.n[i].transpose(Image.ROTATE_90)
+        if self.sender().text()[:2] == "По":
+            self.name = self.name.transpose(Image.ROTATE_270)
+        else:
+            self.name = self.name.transpose(Image.ROTATE_90)
+        self.name.save(self.fname)
+        self.image.setPixmap(QPixmap(self.fname))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
