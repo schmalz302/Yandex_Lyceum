eec25f345b414d6ae6c781d9f6af8e01c0742bfa
buk55 <boltneffe@yandex.ru>
2021-05-01 14:01:44 +0500

    Структура архива. Часть 2

--- 1.py
+++ 1.py
@@ -1,4 +1,22 @@
 from zipfile import ZipFile
+import zipfile
+import os
+
+
+def human_read_format(size):
+    if size < 1024:
+        return f'{round(size)}Б'
+    elif size / 1024 < 1024:
+        a = size / 1024
+        return f'{round(a)}КБ'
+    elif size / (1024 ** 2) < 1024:
+        a = size / (1024 ** 2)
+        return f'{round(a)}МБ'
+    elif size / (1024 ** 3) < 1024:
+        a = size / (1024 ** 3)
+        return f'{round(a)}ГБ'
+
+
 b = {}
 
 
@@ -13,10 +31,17 @@ def bb(a):
 
 with ZipFile('input.zip') as myzip:
     a = list(myzip.namelist())
+    bc = list(myzip.namelist())
+    cc = []
+    cd = []
     for i in range(len(a)):
+        cd = os.path.isfile(myzip.extract(a[i]))
+        cc = human_read_format(myzip.getinfo(a[i]).file_size)
         if a[i][-1] == '/':
             a[i] = a[i][:-1]
-    a = [i.split('/') for i in myzip.namelist()]
+        if cd:
+            a[i] += f'__{cc}'
+    a = [i.split('/') for i in a]
     for i in range(len(a)):
         if a[i][-1] == '':
             a[i] = a[i][:-1]
@@ -26,8 +51,12 @@ with ZipFile('input.zip') as myzip:
 
 def aa(a, b):
     for key in a:
-        print(b * ' ' + key)
+        c = ' '.join(key.split('__')).rstrip()
+        print(b * ' ' + c)
         aa(a[key], b + 2)
 
 
 aa(b, 0)
+
+
+
