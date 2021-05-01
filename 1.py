fab8378fb945683c9a77229953b2c682c1ee2f39
buk55 <boltneffe@yandex.ru>
2021-05-01 14:02:02 +0500

    Что удалить?

--- 1.py
+++ 1.py
@@ -1,8 +1,10 @@
-from zipfile import ZipFile
-import zipfile
 import os
+aa = input('Введите название каталога: ')
+cc = os.listdir(aa)
+f = []
 
 
+# функция для представления размера в нормальном виде
 def human_read_format(size):
     if size < 1024:
         return f'{round(size)}Б'
@@ -17,46 +19,13 @@ def human_read_format(size):
         return f'{round(a)}ГБ'
 
 
-b = {}
-
-
-def bb(a):
-    c = b
-    for i in range(len(a)):
-        if a[i] in c:
-            c = c[a[i]]
-        else:
-            c[a[i]] = {}
-
-
-with ZipFile('input.zip') as myzip:
-    a = list(myzip.namelist())
-    bc = list(myzip.namelist())
-    cc = []
-    cd = []
-    for i in range(len(a)):
-        cd = os.path.isfile(myzip.extract(a[i]))
-        cc = human_read_format(myzip.getinfo(a[i]).file_size)
-        if a[i][-1] == '/':
-            a[i] = a[i][:-1]
-        if cd:
-            a[i] += f'__{cc}'
-    a = [i.split('/') for i in a]
-    for i in range(len(a)):
-        if a[i][-1] == '':
-            a[i] = a[i][:-1]
-    for i in a:
-        bb(i)
-
-
-def aa(a, b):
-    for key in a:
-        c = ' '.join(key.split('__')).rstrip()
-        print(b * ' ' + c)
-        aa(a[key], b + 2)
-
-
-aa(b, 0)
-
-
-
+for i in os.listdir(aa):
+    c = os.path.getsize(f"{aa}\{i}")
+    if os.path.isdir(f"{aa}\{i}"):
+        f.append([len(os.listdir(f"{aa}\{i}")), human_read_format(c), c, i])
+    else:
+        f.append([0, human_read_format(c), c, i])
+f = sorted(f, key=lambda x: x[0])[-10:]
+f = sorted(f, key=lambda x: x[2])[::-1]
+for i in range(len(f)):
+    print(f"{f[i][3]}{(40 - len(str(f[i][3]))) * ' '}-{f[i][1]}")
