8be40c8089abc9978cd8c516b6aa852c5857237d
buk55 <boltneffe@yandex.ru>
2021-05-01 13:45:01 +0500

    Не квадрат-объектив

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QPushButton, QLabel
+from PyQt5.QtWidgets import QColorDialog
 from math import *
 
 
@@ -12,12 +13,14 @@ class Example(QWidget):
         self.btn = QPushButton('Рисовать', self)
         self.edit1 = QLineEdit(self)
         self.edit2 = QLineEdit(self)
+        self.edit3 = QLineEdit(self)
         self.lbl1 = QLabel(self)
         self.lbl2 = QLabel(self)
+        self.lbl3 = QLabel(self)
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 100, 600, 600)
+        self.setGeometry(100, 100, 800, 800)
         self.setWindowTitle('Квадрат-объектив — 2')
 
         self.lbl1.setFixedHeight(30)
@@ -25,16 +28,24 @@ class Example(QWidget):
         self.lbl1.setText('K =')
         self.lbl1.move(20, 20)
         self.edit1.setFixedHeight(30)
-        self.edit1.setFixedWidth(150)
+        self.edit1.setFixedWidth(50)
         self.edit1.move(50, 20)
 
         self.lbl2.setFixedHeight(30)
         self.lbl2.setFixedWidth(30)
         self.lbl2.setText('N =')
-        self.lbl2.move(210, 20)
+        self.lbl2.move(110, 20)
         self.edit2.setFixedHeight(30)
-        self.edit2.setFixedWidth(150)
-        self.edit2.move(240, 20)
+        self.edit2.setFixedWidth(50)
+        self.edit2.move(140, 20)
+
+        self.lbl3.setFixedHeight(30)
+        self.lbl3.setFixedWidth(30)
+        self.lbl3.setText('M =')
+        self.lbl3.move(200, 20)
+        self.edit3.setFixedHeight(30)
+        self.edit3.setFixedWidth(50)
+        self.edit3.move(230, 20)
 
         self.btn.resize(150, 30)
         self.btn.move(400, 20)
@@ -42,32 +53,44 @@ class Example(QWidget):
         self.do_paint = False
 
     def pp(self):
+        self.color = QColorDialog.getColor()
         self.do_paint = True
         self.repaint()
 
+    def p(self, qp):
+        # переменные
+        ed1 = float(self.edit1.text())
+        ed2 = int(self.edit2.text())
+        ed3 = int(self.edit3.text())
+        # переменная для работы с радианами и угол постоянного наклона
+        b = pi / 180
+        ang3 = 360 // ed3
+        # угол наклона
+        c, cc = 1 - ed1, ed1
+        ac = ((cos(ang3 * b) * cc + c) ** 2 + (sin(ang3 * b) * cc) ** 2) ** 0.5
+        angle2 = asin(c * sin(ang3 * b) / ac) / b
+        # угол начала
+        angle = 360 // ed3
+        # рисование
+        a, x, y = 1200 // ed3, 150, 450
+        qp.setPen(QPen(self.color, 2))
+        for i in range(ed2):
+            for j in range(ed3):
+                x2, y2 = x + round(a * cos(angle * b)), y + round(a * -sin(angle * b))
+                qp.drawLine(x, y, x2, y2)
+                x, y = x2, y2
+                angle -= ang3
+            angle %= 360
+            aa = a * (1 - ed1)
+            x, y = x + round(aa * cos(angle * b)), y + round(aa * -sin(angle * b))
+            a *= ac
+            angle -= angle2
+
     def paintEvent(self, event):
         if self.do_paint:
-            ed1 = float(self.edit1.text())
-            ed2 = int(self.edit2.text())
-            b = pi / 180
-            angle2 = atan((1 - ed1) / ed1) / b
-            angle = 90
             qp = QPainter()
             qp.begin(self)
-            a, x, y = 300, 150, 450
-            pen = QPen(Qt.black, 2)
-            qp.setPen(pen)
-            for i in range(ed2):
-                for j in range(4):
-                    x2, y2 = x + round(a * cos(angle * b)), y + round(a * -sin(angle * b))
-                    qp.drawLine(x, y, x2, y2)
-                    x, y = x2, y2
-                    angle -= 90
-                angle %= 360
-                aa = a * (1 - ed1)
-                x, y = x + round(aa * cos(angle * b)), y + round(aa * -sin(angle * b))
-                a = (ed1 * a) / cos(angle2 * b)
-                angle -= angle2
+            self.p(qp)
             qp.end()
             self.do_paint = False
 
